e9854cf128b23e6c77e936dd3c5f6f2312159eb0
choi11000 <choi120792@gmail.com>
2022-03-20 19:39:12 +0900

    first

---
+++ Assets/script/Mage.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mage : MonoBehaviour
+{
+    [SerializeField] float m_speed = 4.0f;
+    [SerializeField] bool m_noBlood = false;
+    [SerializeField] GameObject m_slideDust;
+
+    private Animator m_animator;
+    private Rigidbody2D m_body2d;
+    private Sensor_Mage m_groundSensor;
+    private Sensor_Mage m_wallSensorR1;
+    private Sensor_Mage m_wallSensorR2;
+    private Sensor_Mage m_wallSensorL1;
+    private Sensor_Mage m_wallSensorL2;
+    private bool m_grounded = false;
+    private int m_facingDirection = 1;
+    private int m_currentAttack = 0;
+    private float m_timeSinceAttack = 0.0f;
+    private float m_delayToIdle = 0.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        m_body2d = GetComponent<Rigidbody2D>();
+        m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_Mage>();
+        m_wallSensorR1 = transform.Find("WallSensor_R1").GetComponent<Sensor_Mage>();
+        m_wallSensorR2 = transform.Find("WallSensor_R2").GetComponent<Sensor_Mage>();
+        m_wallSensorL1 = transform.Find("WallSensor_L1").GetComponent<Sensor_Mage>();
+        m_wallSensorL2 = transform.Find("WallSensor_L2").GetComponent<Sensor_Mage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Increase timer that controls attack combo
+        m_timeSinceAttack += Time.deltaTime;
+        /*
+        //Check if character just landed on the ground
+        if (!m_grounded && m_groundSensor.State())
+        {
+            m_grounded = true;
+            m_animator.SetBool("Grounded", m_grounded);
+        }
+
+        //Check if character just started falling
+        if (m_grounded && !m_groundSensor.State())
+        {
+            m_grounded = false;
+            m_animator.SetBool("Grounded", m_grounded);
+        }
+        */
+        // -- Handle input and movement --
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        // Swap direction of sprite depending on walk direction
+        if (inputX > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            m_facingDirection = 1;
+        }
+
+        else if (inputX < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            m_facingDirection = -1;
+        }
+
+        if (inputY > 0)
+        {
+            GetComponent<SpriteRenderer>().flipY = false;
+            m_facingDirection = 1;
+        }
+
+        // Move
+        m_body2d.velocity = new Vector2(inputX * m_speed, inputY * m_speed);
+
+        //Set AirSpeed in animator
+        //m_animator.SetFloat("AirSpeedY", m_body2d.velocity.y);
+
+        //Death
+        if (Input.GetKeyDown("e"))
+        {
+            m_animator.SetBool("noBlood", m_noBlood);
+            m_animator.SetTrigger("Die");
+        }
+
+        //Hurt
+        else if (Input.GetKeyDown("q"))
+            m_animator.SetTrigger("hurt");
+
+        //Attack
+        else if (Input.GetMouseButtonDown(0) && m_timeSinceAttack > 0.25f)
+        {
+            m_currentAttack = 1;
+            m_animator.SetTrigger("Attack" + m_currentAttack);
+
+            // Reset timer
+            m_timeSinceAttack = 0.0f;
+        }
+
+        //Run
+        else if (Mathf.Abs(inputX) > Mathf.Epsilon)
+        {
+            // Reset timer
+            m_delayToIdle = 0.05f;
+            m_animator.SetInteger("AnimState", 1);
+        }
+        else if (Mathf.Abs(inputY) > Mathf.Epsilon)
+        {
+            // Reset timer
+            m_delayToIdle = 0.05f;
+            m_animator.SetInteger("AnimState", 1);
+        }
+        //Idle
+        else
+        {
+            // Prevents flickering transitions to idle
+            m_delayToIdle -= Time.deltaTime;
+            if (m_delayToIdle < 0)
+                m_animator.SetInteger("AnimState", 0);
+        }
+    }
+
+    // Animation Events
+    // Called in end of roll animation.
+
+}
